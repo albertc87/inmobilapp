dfd152630ce4b07054244b9385c6d96098276de5
albertc87 <ac@Albertos-MacBook-Pro.local>
2014-11-14 12:49:30 -0500

    Primer Commit
    A pelo

---
+++ inmobilapp/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  inmobilapp
+//
+//  Created by Alberto Castro on 13/11/14.
+//  Copyright (c) 2014 Alberto Castro. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController {
+
+    @IBOutlet var map: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
