7a7fa31d1b0e9b45ff458696a1a3f97021c6271c
albertc87 <ac@Albertos-MacBook-Pro.local>
2014-11-15 11:18:13 -0500

    añadidas imágenes
    Actualizadas referencias

---
+++ Inmueble.swift
@@ -0,0 +1,18 @@
+//
+//  Inmueble.swift
+//  inmobilapp
+//
+//  Created by Alberto Castro on 14/11/14.
+//  Copyright (c) 2014 Alberto Castro. All rights reserved.
+//
+
+import Foundation
+class Inmueble{
+    var id: Int = 0
+    var description: String = ""
+    
+    init(){
+    
+    }
+    
+}
