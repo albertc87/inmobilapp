877b86c3351c7a6240f5bbf48241738fb14bc274
byronsliger <byron.sliger@gmail.com>
2014-11-18 13:22:24 -0500

    DetallesViewController
    Campos de detalles

--- Inmueble.swift
+++ Inmueble.swift
@@ -15,6 +15,20 @@ class Inmueble : NSObject, MKAnnotation{
     //var description: String
     var coordinate: CLLocationCoordinate2D
     var title: String!
+    var address: String!//
+    var reference: String!//
+    var numberBathrooms : Int!//
+    var numberBedrooms : Int!//
+    var price: Int!//
+    var administrationCost: Int!//
+    var neighborhood: String!
+    var haveParking: Bool!
+    var haveGasService: Bool!
+    var haveSurveillanceService : Bool! //porteria
+    var type: String!
+    var level: String!// Estrato
+    var area: Int!
+    var typeKitchen : String!//Tipo cocina
     
     init(coordinate: CLLocationCoordinate2D, title: String) {
         self.coordinate = coordinate
