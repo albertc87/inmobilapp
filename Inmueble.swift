2d31f8ef3911fe007a5bd4cdaf5230de91adfc98
albertc87 <ac@Albertos-MacBook-Pro.local>
2014-11-17 22:58:41 -0500

    inmueble

--- Inmueble.swift
+++ Inmueble.swift
@@ -10,6 +10,27 @@ import Foundation
 class Inmueble{
     var id: Int = 0
     var description: String = ""
+    var address: String = ""
+    var reference: String = ""
+    var longitude: Double = 0
+    var latitude: Double = 0
+    var numberBathrooms : Int = 0
+    var numberBedrooms : Int = 0
+    var price: Int = 0
+    var neighborhood: String = ""
+    var administrationCost: Int = 0
+    var haveParking: Bool = false
+    var haveGasService: Bool = false
+    var haveSurveillanceService : Bool = false //porteria
+    var type: String = ""
+    var level: String = ""// Estrato
+    var area: Int = 0
+    var typeKitchen : String = ""//Tipo cocina
+    //TODO: imagenes y video
+    //cargar los datos a partir del json, consultar los que estan en el radio
+    func loadFromJSON(){
+    
+    }
     
     init(){
     
